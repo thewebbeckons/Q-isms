c236e56d00db407b1d80702fac01f4878d7bc6f1
Jesse Beck <thewebbeckons@users.noreply.github.com>
2024-11-10 19:39:19 -0500

    Feature: add twitch link

--- components/AppFooter.vue
+++ components/AppFooter.vue
@@ -1,9 +1,21 @@
 <template>
   <div
-    class="flex flex-row flex-shrink-0 items-center justify-center px-8 py-5"
+    class="flex flex-col flex-shrink-0 items-center justify-center px-8 py-2"
   >
     <p class="text-slate-500 text-center">
       From the brilliant mind of the one of a kind Q
     </p>
+    <div>
+      <UTooltip text="Twitch" :popper="{ placement: 'top' }">
+        <UButton
+          variant="link"
+          color="purple"
+          to="https://www.twitch.tv/qwinnlucky13"
+          size="lg"
+        >
+          <UIcon name="i-mdi-twitch" class="w-6 h-6" />
+        </UButton>
+      </UTooltip>
+    </div>
   </div>
 </template>
